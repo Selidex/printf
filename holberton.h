915c901e5adebe5ca530807b34b2645ebd9a4657
Selidex <justindparnell@yahoo.com>
2020-10-26 22:58:43 +0000

    holberton.h updated for itobi

--- holberton.h
+++ holberton.h
@@ -22,10 +22,11 @@ typedef struct placement
 void icount(char *buffer, struct placement *place);
 int _write(char *buffer, int n);
 placement *placeholder(void);
+int _getflags(const char *format, char *buffer, placement *place, va_list ap);
 int _printf(const char *format, ...);
 int choose(const char c, va_list ap, char *buffer, placement *place);
 int printint(int n, char *buffer, placement *place);
 int printchar(char c, char *buffer, placement *place);
 int printstr(char *str, char *buffer, placement *place);
-
+int itob(unsigned int n, char *buffer, placement *place);
 #endif /* HOLBERTON_H */
