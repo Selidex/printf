5b6fce7c1cb517fa80e644bb01aafd218f53eb73
Selidex <justindparnell@yahoo.com>
2020-10-27 02:47:02 +0000

    Adding r prototype

--- holberton.h
+++ holberton.h
@@ -26,6 +26,7 @@ int _getflags(const char *format, char *buffer, placement *place, va_list ap);
 int _printf(const char *format, ...);
 int choose(const char c, va_list ap, char *buffer, placement *place);
 int rot(char *str, char *buffer, placement *place);
+int rev(char *str, char *buffer, placement *place);
 int printint(int n, char *buffer, placement *place);
 int printchar(char c, char *buffer, placement *place);
 int printstr(char *str, char *buffer, placement *place);
